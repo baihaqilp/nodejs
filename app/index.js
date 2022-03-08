858114468c9663de144a1183b86ec93c56a200bc
github-ragil <work.ragil@gmail.com>
2022-03-08 21:02:40 +0700

    update

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
-
+app.use(express.static(__dirname + '/'));
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log('Server started at http://192.168.200.16:' + port);
 
