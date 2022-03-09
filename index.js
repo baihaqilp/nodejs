9d203fc2b591a6c61b30b9f68650ad78218e0716
github-ragil <work.ragil@gmail.com>
2022-03-09 16:15:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5001-5010;
+const port = process.env.PORT || 5001;
 
 // sendFile will go here
 app.get('/', function(req, res) {
@@ -9,5 +9,5 @@ app.get('/', function(req, res) {
 });
 app.use(express.static(__dirname + '/'));
 app.listen(port);
-console.log('Server started at http://192.168.200.16:' + port);
+console.log('Server started at http://192.168.200.16:';
 
