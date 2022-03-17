4db83884a3fc4435f6c4795b9a9a415a1bb251b3
github-ragil <work.ragil@gmail.com>
2022-03-17 17:50:55 +0700

    Update telegram-notif.sh

--- telegram-notif.sh
+++ telegram-notif.sh
@@ -1,4 +1,4 @@
-curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode="HTML" -d text="<b>Project</b> : Hackathon BCA by HAI MP 06 <br> \
+curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode="HTML" -d text="<p><b>Project</b> : Hackathon BCA by HAI MP 06 <br> \
 <b>Branch</b>: master <br> \
 <b>Build </b> : OK <br> \
-<b>Test suite</b> = Passed"
+<b>Test suite</b> = Passed </p> "
