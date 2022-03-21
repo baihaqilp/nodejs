3e9bff32d03a11e2dcedf6fef1126be2c7aacbeb
github-ragil <work.ragil@gmail.com>
2022-03-21 21:53:03 +0700

    Update telegram-started.sh

--- telegram-started.sh
+++ telegram-started.sh
@@ -1,3 +1,2 @@
 curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode="HTML" -d text="<b>Project</b> : Hackathon BCA by HAI MP 06 \
-<b>Build </b> : Start \
-<b>Test suite</b> = Testing Nodejs App "
+<b>Build </b> : Starting Job Nodejs"
